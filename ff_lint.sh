088f04fd4612150dfc5c3b50dce3eea5d46346e2
Janko <janko.huic@gmail.com>
2025-01-27 13:34:05 +0100

    lint script generalized for use with .buildignore

--- ff_lint.sh
+++ ff_lint.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+IGNORE_FILE=".buildignore"
+
+process_ignore_patterns() {
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        [[ -z "$line" || "$line" == \#* ]] && continue
+        
+        if [[ "$line" == */ ]]; then
+            echo -n " \"$line*\""
+        else
+            echo -n " \"$line\""
+        fi
+    done < "$IGNORE_FILE"
+}
+
 cp manifest_firefox.json manifest.json
 
-web-ext lint --ignore-files "build*" "bs/" "*.sh" "options2.js" "coursefiles/"
+LINT_COMMAND="web-ext lint --ignore-files $(process_ignore_patterns)"
+
+evail $LINT_COMMAND
 
 cp manifest_chrome.json manifest.json
